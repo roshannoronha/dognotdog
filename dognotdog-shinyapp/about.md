d08ac29a994f83774551dcd1000771407da16582
Roshan Noronha <7717622+roshannoronha@users.noreply.github.com>
2019-09-24 18:23:42 -0700

    Add files via upload

---
+++ dognotdog-shinyapp/about.md
@@ -0,0 +1,11 @@
+This app takes an image and uses machine learning to classify it as a "dog"" or a "not dog". The model was trained using the <a href="https://keras.io/" target="_blank">Keras library</a> with <a href="https://www.tensorflow.org/" target="_blank">Tensorflow</a> as the backend.
+
+For more information about the author and links to the code please use the links below.
+
+### Code
+<a href="https://github.com/roshannoronha" target="_blank">Github</a>
+
+### Author
+<a href="https://roshannoronha.github.io/" target="_blank">Website</a>
+
+<a href="https://www.linkedin.com/in/roshananoronha/" target="_blank">LinkedIn</a>
---
+++ dognotdog-shinyapp/app.R
@@ -0,0 +1,91 @@
+#install packages if missing
+if (!require(shiny)) {
+  install.packages("shiny")
+}
+
+if (!require(imager)) {
+  install.packages("imager")
+}
+
+if (!require(reticulate)) {
+  install.packages("reticulate")
+}
+
+if (!require(markdown)) {
+  install.packages("markdown")
+}
+
+#load required packages
+library(shiny)
+library(imager)
+library(reticulate)
+library(markdown)
+
+#create virtual env
+virtualenv_install("env", packages = c("Pillow", "keras", "tensorflow"), ignore_installed = FALSE)
+
+#import python libraries
+PIL <- import(module = "PIL")
+keras <- import("keras")
+
+#load weight file
+trainedModel <- keras$models$load_model("V3-transferlearningcatsanddogs.h5")
+
+#define UI
+ui <- fluidPage(
+  
+  titlePanel("Dog Not a Dog"),
+  
+  navbarPage("", 
+             tabPanel("App",
+                sidebarLayout(
+                  sidebarPanel(
+                    fileInput("file", "Upload your Picture", accept = c('image/png', 'image/jpeg'))
+                  ),
+                  
+                  mainPanel(
+                    
+                    plotOutput("image"),
+                    textOutput("text")
+                  )
+                )),
+             
+             tabPanel("About",
+                      includeMarkdown("about.md"))
+  )
+)
+
+server <- function(input, output) {
+  
+  observeEvent(input$file, {
+    
+    #don't remove
+    userImage <- input$file
+    
+    output$image <- renderPlot({
+      
+      plot(load.image(userImage$datapath), axes = FALSE)
+    })
+    
+    output$text <- renderText(defDog(userImage$datapath))
+  })
+  
+  defDog <- function(imagePath) {
+    
+    testImage <- keras$preprocessing$image$load_img(path=imagePath, target_size = r_to_py(c(224L, 224L)))
+    testImage <- keras$preprocessing$image$img_to_array(testImage, data_format = "channels_last")
+    testImage <- keras$backend$expand_dims(testImage, axis = 0L)
+    
+    prediction <- trainedModel$predict(testImage, steps = 1L)
+    
+    if(which(prediction == max(prediction)) == 1) {
+      return ("Not a dog")
+      
+    } else {
+      
+      return ("This is a dog!")
+    }
+  }
+}
+
+shinyApp(ui = ui, server = server)
